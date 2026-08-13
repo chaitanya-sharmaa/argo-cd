b9273775691c6161f74161dc6c20578eab44db31
Chaitanya <>
2026-08-13 12:26:42 +0100

    fix: use free-tier eligible t3.micro instances

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,5 +13,5 @@ variable "cluster_name" {
 variable "node_instance_type" {
   description = "EC2 instance type for EKS worker nodes"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.micro"
 }
